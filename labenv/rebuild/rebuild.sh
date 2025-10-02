bb5aae9aa886904a6b274de218c1817c46bc8a35
Peter Árpád CSURGAY <csurgay@gmail.com>
2025-10-02 21:10:00 +0200

    Fix /etc/hosts populate in rebuild script

--- labenv/rebuild/rebuild.sh
+++ labenv/rebuild/rebuild.sh
@@ -1,78 +1,84 @@
-printf "\n##### Rebuild sctipt starting\n\n"
+log("### Rebuild sctipt starting")
 
 podman rmi -fa
 
-printf "\n### All root images and containers removed\n\n"
+log("### All root images and containers removed")
 
 podman network create ansible
 
-printf "\n### Podman network created\n\n"
+log("### Podman network created")
 
 cd ../controlnode
 ./build.sh
 
-printf "\n### Controlnode image is built\n\n"
+log("### Controlnode image is built")
 
 podman tag controlnode docker.io/csurgay/controlnode:latest
 podman push docker.io/csurgay/controlnode:latest
 
-printf "\n### Controlnode images pushed to public registry\n\n"
+log("### Controlnode images pushed to public registry")
 
 ./run.sh
 
-printf "\n### Controlnode container is running\n\n"
+log("### Controlnode container is running")
 
 cd ../managedhost
 ./build.sh
 
-printf "\n### Managedhost image is built\n\n"
+log("### Managedhost image is built")
 
 podman tag managedhost docker.io/csurgay/managedhost:latest
 podman push docker.io/csurgay/managedhost:latest
 
-printf "\n### Managedhost image pushed to public registry\n\n"
+log("### Managedhost image pushed to public registry")
 
 ./run.sh
 
-printf "\n### Managedhost containers are running\n\n"
+log("### Managedhost containers are running")
 
 cd ../rebuild
 
 podman exec ansible dnf install -y git
 
-printf "\n### Git is installed in Controlnode container\n\n"
+log("### Git is installed in Controlnode container")
 
 podman exec -w=/root ansible git clone https://github.com/csurgay/ansible-training.git
 
-printf "\n### Training lab is pulled from Git to Controlnode container\n\n"
+log("### Training lab is pulled from Git to Controlnode container")
 
 podman exec ansible dnf install -y ansible
 
-printf "\n### Ansible is installed in Controlnode container\n\n"
+log("### Ansible is installed in Controlnode container")
 
-podman exec ansible bash -c 'printf "[defaults]\ninterpreter_python=auto_silent\nlog_path=ansible.log\n\n" > /root/ansible-training/labenv/ansible.cfg'
+podman exec ansible bash -c 'printf "[defaults]\ninterpreter_python=auto_silent\nlog_path=ansible.log) > /root/ansible-training/labenv/ansible.cfg'
 
-printf "\n### Ansible config is saved in Controlnode container\n\n"
+log("### Ansible config is saved in Controlnode container")
 
-podman exec ansible bash -c 'printf "[hosts]\nhost1\nhost2\nhost3\n\n" > /root/ansible-training/labenv/inventory'
+podman exec ansible bash -c 'printf "[hosts]\nhost1\nhost2\nhost3) > /root/ansible-training/labenv/inventory'
 
-printf "\n### Ansible Inventory is saved in Controlnode container\n\n"
+log("### Ansible Inventory is saved in Controlnode container")
 
-for i in {1..3}; do str="$(podman network inspect ansible | grep -A5 host$i | grep ip | sed s/\ //g | sed s/.*:\"// | sed s'/\/.*//') host$i"; podman exec ansible bash -c "printf $str >> /etc/hosts"; done
+for i in {1..3}; do str="$(podman network inspect ansible | grep -A5 host$i | grep ip | sed s/\ //g | sed s/.*:\"// | sed s'/\/.*//') host$i"; podman exec ansible bash -c "echo $str >> /etc/hosts"; done
 
-printf "\n### Managed hosts IP addresses is saved in Controlnode container\n\n"
+log("### Managed hosts IP addresses is saved in Controlnode container")
 
 podman exec ansible ssh-keygen -t rsa -b 4096 -f ~/.ssh/id_rsa -P ""
 
-printf "\nSSH key is generated for root in Controlnode container\n\n"
+log("### SSH key is generated for root in Controlnode container")
 
 podman exec ansible bash -c 'printf "root" > /root/pass'
 
 for i in {1..3}; do podman exec ansible sshpass -f /root/pass ssh-copy-id host$i; done
 
-printf "\n### SSH key is copied into Managedhost containers\n\n"
+log("### SSH key is copied into Managedhost containers")
 
 podman exec ansible ansible -i /root/ansible-training/labenv/inventory hosts -m ping
 
-printf "\n### Ansible accessing Managedhosts is tested OK\n\n"
+log("### Ansible accessing Managedhosts is tested OK")
 
+log() {
+	printf("\n##############################\n")
+	prinff($1)
+	printf("\n##############################\n")
+	printf("\n")
+}
