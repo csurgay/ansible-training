b00ec3e505ab671cbc5f99947fb8226bb914115e
Peter Csurgay <csurgay@gmail.com>
2025-10-02 13:07:38 +0200

    Fix comments in rebuild script

--- labenv/rebuild/rebuild.sh
+++ labenv/rebuild/rebuild.sh
@@ -1,78 +1,78 @@
-echo "##### Rebuild sctipt starting"
+printf "\n##### Rebuild sctipt starting\n"
 
 podman rmi -fa
 
-echo "### All root images and containers removed"
+printf "\n### All root images and containers removed\n"
 
 podman network create ansible
 
-echo "### Podman network created"
+printf "\n### Podman network created\n"
 
 cd ../controlnode
 ./build.sh
 
-echo "### Controlnode image build"
+printf "\n### Controlnode image is built\n"
 
 podman tag controlnode docker.io/csurgay/controlnode:latest
 podman push docker.io/csurgay/controlnode:latest
 
-echo "### Controlnode images pushed to public registry"
+printf "\n### Controlnode images pushed to public registry\n"
 
 ./run.sh
 
-echo "### Controlnode container is running"
+printf "\n### Controlnode container is running\n"
 
 cd ../managedhost
 ./build.sh
 
-echo "### Managedhost image built"
+printf "\n### Managedhost image is built\n"
 
 podman tag managedhost docker.io/csurgay/managedhost:latest
 podman push docker.io/csurgay/managedhost:latest
 
-echo "### Managedhost image pushed to public registry"
+printf "\n### Managedhost image pushed to public registry\n"
 
 ./run.sh
 
-echo "### Managedhost containers are running"
+printf "\n### Managedhost containers are running\n"
 
 cd ../rebuild
 
 podman exec ansible dnf install -y git
 
-echo "### Git is installed in Controlnode container"
+printf "\n### Git is installed in Controlnode container\n"
 
 podman exec -w=/root ansible git clone https://github.com/csurgay/ansible-training.git
 
-echo "### Training lab is pulled from Git to Controlnode container"
+printf "\n### Training lab is pulled from Git to Controlnode container\n"
 
 podman exec ansible dnf install -y ansible
 
-echo "### Ansible is installed in Controlnode container"
+printf "\n### Ansible is installed in Controlnode container\n"
 
 podman exec ansible bash -c 'printf "[defaults]\ninterpreter_python=auto_silent\nlog_path=ansible.log\n" > /root/ansible-training/labenv/ansible.cfg'
 
-echo "### Ansible config is saved in Controlnode container"
+printf "\n### Ansible config is saved in Controlnode container\n"
 
 podman exec ansible bash -c 'printf "[hosts]\nhost1\nhost2\nhost3\n" > /root/ansible-training/labenv/inventory'
 
-echo "### Ansible Inventory is saved in Controlnode container"
+printf "\n### Ansible Inventory is saved in Controlnode container\n"
 
-for i in {1..3}; do str="$(podman network inspect ansible | grep -A5 host$i | grep ip | sed s/\ //g | sed s/.*:\"// | sed s'/\/.*//') host$i"; podman exec ansible bash -c "echo $str >> /etc/hosts"; done
+for i in {1..3}; do str="$(podman network inspect ansible | grep -A5 host$i | grep ip | sed s/\ //g | sed s/.*:\"// | sed s'/\/.*//') host$i"; podman exec ansible bash -c "printf $str >> /etc/hosts"; done
 
-echo "### Managed hosts IP addresses is saved in Controlnode container"
+printf "\n### Managed hosts IP addresses is saved in Controlnode container\n"
 
 podman exec ansible ssh-keygen -t rsa -b 4096 -f ~/.ssh/id_rsa -P ""
 
-echo "SSH key is generated for root in Controlnode container"
+printf "\nSSH key is generated for root in Controlnode container\n"
 
 podman exec ansible bash -c 'printf "root" > /root/pass'
 
 for i in {1..3}; do podman exec ansible sshpass -f /root/pass ssh-copy-id host$i; done
 
-echo "### SSH key is copied into Managedhost containers"
+printf "\n### SSH key is copied into Managedhost containers\n"
 
 podman exec ansible ansible -i /root/ansible-training/labenv/inventory hosts -m ping
 
-echo "### Ansible accessing Managedhosts is tested OK"
+printf "\n### Ansible accessing Managedhosts is tested OK\n"
 
