471104538a9ac39027e26df49f31942f84fe1f04
Peter Csurgay <csurgay@gmail.com>
2025-10-07 15:26:52 +0200

    Create podman network ansible in run.sh

--- labenv/rootless/run.sh
+++ labenv/rootless/run.sh
@@ -1,5 +1,4 @@
 podman network create ansible
-podman run --name ansible --hostname ansible -d --network ansible docker.io/csurgay/rootless_controlno
-de
+podman run --name ansible --hostname ansible -d --network ansible docker.io/csurgay/rootless_controlnode
 for i in {1..3}; do podman run --name host$i --privileged --hostname host$i -d -v /sys/fs/cgroup:/sys/fs/cgroup:ro -p 282$i:22 -p 888$i:80 --network ansible --mac-address 10:10:10:10:10:1$i docker.io/csurgay/rootless_managedhost; done
 
