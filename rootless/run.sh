61f1c691587194f6be7050eb7af68e3fca05702b
Peter Csurgay <csurgay@gmail.com>
2025-10-07 10:21:42 +0200

    remove bogus rootless run.sh

--- rootless/run.sh
+++
@@ -1,3 +0,0 @@
-podman run --name ansible --hostname ansible -d --network ansible docker.io/csurgay/rootless_controlnode
-for i in {1..3}; do podman run --name host$i --privileged --hostname host$i -d -v /sys/fs/cgroup:/sys/fs/cgroup:ro -p 292$i:22 -p 898$i:80 --network ansible --mac-address 10:10:10:10:10:1$i docker.io/csurgay/rootless_managedhost; done
-
