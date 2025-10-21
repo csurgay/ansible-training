2aa42593a9e3aea5768102d80a376a05513fc2ef
Peter Csurgay <csurgay@gmail.com>
2025-10-21 12:45:42 +0200

    Pimp rootless run.sh

--- labenv/rootless/run.sh
+++ labenv/rootless/run.sh
@@ -1,15 +1,27 @@
 #!/bin/bash
+
+echo "### Create podman network ansible if needed"
+
 podman network exists ansible
 if [ $? -eq 0 ]
 	then echo "podman network ansible already created"
 	else podman network create ansible
 fi
+
+echo "### Run rootless Control Node"
+
 podman run --name ansible --hostname ansible -d --network ansible docker.io/csurgay/rootless_controlnode
+
+echo "### Run rootless Managed Hosts"
+
 for i in {1..3}; do podman run --name host$i --privileged --hostname host$i -d -v /sys/fs/cgroup:/sys/fs/cgroup:ro -p 282$i:22 -p 888$i:80 --network ansible --mac-address 10:10:10:10:10:1$i docker.io/csurgay/rootless_managedhost; done
 
-podman exec -it -w /home/local ansible rm -rvf ansible-training
+echo "### Clone latest Ansible Training Lab"
 
+podman exec -it -w /home/local ansible rm -rvf ansible-training
 podman exec -it -u local -w /home/local ansible git clone https://github.com/csurgay/ansible-training.git
+
+echo "### Enter Control Node for lessons"
 
 podman exec -it -u local -w /home/local/ansible-training/lessons ansible /bin/bash
 
