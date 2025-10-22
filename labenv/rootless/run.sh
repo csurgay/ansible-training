3330cf23e4fc517bf20bf9b85f3caf96e9196596
Peter Csurgay <csurgay@gmail.com>
2025-10-22 17:11:32 +0200

    Rootless also from sinle ansible_node image

--- labenv/rootless/run.sh
+++ labenv/rootless/run.sh
@@ -1,6 +1,24 @@
 #!/bin/bash
 
-echo "### Create podman network ansible if needed"
+#!/bin/bash
+
+log() {
+	if [ $? -eq 0 ]
+	then
+		printf "\n##################################################\n"
+		printf "Success: $1"
+		printf "\n##################################################\n"
+		printf "\n"
+	else
+		printf "\n##################################################\n"
+		printf "ERROR: $1"
+		printf "\n##################################################\n"
+		printf "\n"
+		exit 1
+	fi
+}
+
+log  "Rootless script started"
 
 podman network exists ansible
 if [ $? -eq 0 ]
@@ -8,29 +26,66 @@ if [ $? -eq 0 ]
 	else podman network create ansible
 fi
 
-echo "### Remove all rootless containers"
+log  "Create podman network ansible if needed"
 
 podman rm -fa
 
-echo "### Run rootless Control Node"
+log "All rootless containers removed"
 
-podman run --name ansible --hostname ansible -d --network ansible docker.io/csurgay/rootless_controlnode
+podman run --name ansible --hostname ansible -d --network ansible docker.io/csurgay/ansible_node
 
-echo "### Run rootless Managed Hosts"
+log "Rootless Control Node running"
 
-for i in {1..3}; do podman run --name host$i --privileged --hostname host$i -d -v /sys/fs/cgroup:/sys/fs/cgroup:ro -p 282$i:22 -p 888$i:80 --network ansible --mac-address 10:10:10:10:10:1$i docker.io/csurgay/rootless_managedhost; done
+for i in {1..3}; do podman run --name host$i --privileged --hostname host$i -d -v /sys/fs/cgroup:/sys/fs/cgroup:ro -p 282$i:22 -p 888$i:80 --network ansible --mac-address 10:10:10:10:10:1$i docker.io/csurgay/ansible_node; done
 
-echo "### Give local users NOPASSWD sudo"
+log "Rootless Managed Hosts running"
+
+podman exec -it ansible bash -c "useradd local"
+for i in {1..3}; do podman exec -it host$i bash -c "useradd local"; done
+
+log "User local added to all containers"
+
+podman exec -it ansible bash -c "echo 'local:local' | chpasswd"
+for i in {1..3}; do podman exec -it host$i bash -c "echo 'local:local' | chpasswd"; done
+
+log "Password for users set up"
 
 podman exec -it -w /home/local ansible bash -c "echo 'local ALL=(ALL) NOPASSWD: ALL' > /etc/sudoers.d/local"
 for i in {1..3}; do podman exec -it -w /home/local host$i bash -c "echo 'local ALL=(ALL) NOPASSWD: ALL' > /etc/sudoers.d/local"; done
 
-echo "### Clone latest Ansible Training Lab"
+log "Local users NOPASSWD sudo granted"
+
+podman exec -u local ansible bash -c 'ssh-keygen -t rsa -b 4096 -f ~/.ssh/id_rsa -P ""'
+
+log "Local user ssh key in Control Node created"
+
+podman exec -u local ansible bash -c 'printf "local" > /home/local/pass'
+for i in {1..3}; do podman exec -u local ansible sshpass -f /home/local/pass ssh-copy-id host$i; done
+
+log "SSH key is copied into Managedhost containers"
+
+podman exec -u local -w=/home/local ansible git clone https://github.com/csurgay/ansible-training.git
+
+log "Training lab is pulled from Git to Controlnode container"
+
+podman cp ansible.cfg ansible:/home/local/ansible-training/labenv/rootful/
+
+log "Ansible config is saved in Controlnode container"
+
+podman cp host_inventory ansible:/home/local/ansible-training/labenv/rootful/
+
+log "Ansible Inventory is saved in Controlnode container"
 
 podman exec -it -w /home/local ansible rm -rvf ansible-training
 podman exec -it -u local -w /home/local ansible git clone https://github.com/csurgay/ansible-training.git
 
-echo "### Enter Control Node for lessons"
+log "Latest Ansible Training Lab cloned"
+
+podman exec -u local -w /home/local/ansible-training/labenv/rootless ansible ansible myhosts -m ping
+
+log "Ansible accessing Managedhosts is tested OK"
+
+log "Entring Control Node for lessons"
 
 podman exec -it -u local -w /home/local/ansible-training/lessons ansible /bin/bash
 
