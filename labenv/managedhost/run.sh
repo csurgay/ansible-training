fc58b44cc5277547443a38e76d5a7f8ec88cc0b6
Peter Csurgay <csurgay@gmail.com>
2025-10-22 11:27:03 +0200

    Fix chrony on rootful

--- labenv/managedhost/run.sh
+++ labenv/managedhost/run.sh
@@ -1,4 +1,4 @@
-for i in {1..3}; do podman run --name host$i --hostname host$i -d --privileged -v /sys/fs/cgroup:/sys/fs/cgroup:ro -p 202$i:22 -p 808$i:80 --network ansible --mac-address 10:10:10:10:10:1$i docker.io/csurgay/managedhost; done
+for i in {1..3}; do podman run --name host$i --hostname host$i -d --privileged --cap-add=ALL -v /sys/fs/cgroup:/sys/fs/cgroup:ro -p 202$i:22 -p 808$i:80 --network ansible --mac-address 10:10:10:10:10:1$i docker.io/csurgay/managedhost; done
 
 sleep 3
 
